2210816b1b9750c40b76ea71dd3bdbb5a2358e93
Desy Ayurianti <desii.ayurianti@gmail.com>
2023-03-06 10:09:30 +0700

    firstcommit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfilController;
+use App\Http\Controllers\KuliahController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('layouts.template');
+});
+
+//Dashboaard
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dash');
+
+
+//Profile
+Route::get('/profile', [ProfilController::class, 'index'])->name('prof');
+
+//Pengalaman 
+Route::get('/pengalaman-kuliah', [KuliahController::class, 'index'])->name('pk');
