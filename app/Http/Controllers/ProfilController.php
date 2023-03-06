155e03ec8c3a0049f2fb8119b6526d7402d26d3c
= mariafadilla15 <= mariafadilla15@gmail.com>
2023-03-07 00:53:17 +0700

    dilla

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ProfilController extends Controller
 {
-    //
+    //profil ayu-dilla
     public function index(){
         return view('profile')
         ->with('nama', 'Desy Ayurianti')
@@ -14,21 +14,11 @@ class ProfilController extends Controller
         ->with('kelas', 'TI-2G')
         ->with('jurusan', 'D4 Teknik Informatika')
         ->with('asal', 'Sumenep')
-<<<<<<< HEAD
         
         ->with('nama1', 'Maria Fadilla')
         ->with('nim1', '2141720063')
         ->with('kelas1', 'TI-2G')
         ->with('jurusan1', 'D4 Teknik Informatika')
         ->with('asal1', 'Kediri');
-=======
-
-
-        ->with('nama2', 'Maria Fadilla')
-        ->with('nim2', '2141720063')
-        ->with('kelas2', 'TI-2G')
-        ->with('jurusan2', 'D4 Teknik Informatika')
-        ->with('asal2', 'Kediri');
->>>>>>> 0626d51c77810f343a9ed5ed9f3a785c0797dbd7
     }
 }
